19d447126afd1471812f9c876d51e7a42ffb8ae5
Nate Palmer <1745731+tympaniplayer@users.noreply.github.com>
2025-03-18 11:25:36 -0400

    Add report report
    * Add report reasons dictionary
    * Add report reasons as required options to report command
    * Add alis-list command for report reasons
    * Make user required

---
+++ Reported/Constants.cs
@@ -0,0 +1,15 @@
+namespace Reported;
+
+public static class Constants
+{
+   public static Dictionary<string, string> ReportReasons = new()
+   {
+      { "NA", "Negative Attitude" },
+      { "VA", "Verbal Abuse" },
+      { "AFK", "AFK" },
+      { "IG", "Intentional Griefing" },
+      { "HS", "Hate Speech" },
+      { "CH", "Cheating" },
+      { "ON", "Offensive Name" }
+   };
+}
