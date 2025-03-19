b028209b477454e3089762192a57c2f6dee27d31
Nate <1745731+tympaniplayer@users.noreply.github.com>
2025-03-18 20:00:14 -0400

    Add appeal and why commands (#9)

--- Reported/Commands.cs
+++ Reported/Commands.cs
@@ -20,7 +20,7 @@ public static class Commands
             .WithName("reason")
             .WithDescription("The reason for reporting")
             .WithRequired(true);
-        foreach (var reportReason in Constants.ReportReasons)
+        foreach (var reportReason in Constants.ReportReasons.Where(keyValuePair => keyValuePair.Key != "DU"))
         {
             reportReasonBuilder.AddChoice(reportReason.Value, reportReason.Key);
         }
@@ -44,6 +44,24 @@ public static class Commands
         new SlashCommandBuilder()
             .WithName("alias-list")
             .WithDescription("A list of aliases that can be used when reporting")
+            .WithContextTypes(InteractionContextType.PrivateChannel,
+                InteractionContextType.BotDm,
+                InteractionContextType.Guild)
+            .Build();
+
+    public static SlashCommandProperties WhyReportedCommand() =>
+        new SlashCommandBuilder()
+            .WithName("why-reported")
+            .WithDescription("Stats on the reasons you were reported")
+            .WithContextTypes(InteractionContextType.PrivateChannel,
+                InteractionContextType.BotDm,
+                InteractionContextType.Guild)
+            .Build();
+
+    public static SlashCommandProperties AppealCommand() =>
+        new SlashCommandBuilder()
+            .WithName("appeal")
+            .WithDescription("Appeal to the bot to have a report removed")
             .WithContextTypes(InteractionContextType.PrivateChannel,
                 InteractionContextType.BotDm,
                 InteractionContextType.Guild)
